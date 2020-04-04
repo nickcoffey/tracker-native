76464364c2d08dffa78edd5f1e5efff25cd6c7bb
Nick Coffey <njc3n3@gmail.com>
2020-04-04 10:39:13 -0500

    added react-native-picker styles

--- screens/CurrentWorkoutScreen/CurrentWorkoutSelectExercise.tsx
+++ screens/CurrentWorkoutScreen/CurrentWorkoutSelectExercise.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {useQuery} from '@apollo/react-hooks';
-import {Text} from 'react-native-elements';
+import {Text, Icon} from 'react-native-elements';
+import RNPickerSelect from 'react-native-picker-select';
 
 import {
   CategoriesWithExercisesData,
@@ -18,43 +19,65 @@ const CurrentWorkoutSelectExercise = () => {
   >();
   const [selectedExercise, setSelectedExercise] = useState('');
 
-  const styles = StyleSheet.create({
-    picker: {
-      borderColor: 'black',
+  const baseStyles = StyleSheet.create({
+    styles: {
+      textAlign: 'center',
+      paddingHorizontal: 10,
+      paddingRight: 30, // to ensure the text is never behind the icon
+    },
+  });
+
+  const pickerStyles = StyleSheet.create({
+    inputIOS: {
+      ...baseStyles.styles,
+      paddingVertical: 10,
       borderWidth: 1,
+    },
+    inputAndroid: {
+      ...baseStyles.styles,
+      paddingVertical: 8,
+      borderWidth: 0.5,
+    },
+  });
+
+  const styles = StyleSheet.create({
+    label: {
+      fontSize: 16,
     },
   });
 
   return (
     <>
-      {/* <Text>Category</Text>
+      <Text style={styles.label}>Category</Text>
       <RNPickerSelect
-        style={styles.picker}
-        onValueChange={(value) => {
+        style={pickerStyles}
+        onValueChange={value => {
           setSelectedCategory(value);
           setSelectedExercise('');
         }}
         items={
-          data?.categories.map((category) => ({
+          data?.categories.map(category => ({
             label: category.name,
             value: category,
           })) || []
         }
         placeholder={{label: 'Select a category', value: null}}
+        Icon={() => <Icon type="material" name="expand-more" size={40} />}
       />
-      <Text>Exercise</Text>
+      <Text style={styles.label}>Exercise</Text>
       <RNPickerSelect
         value={selectedExercise}
-        onValueChange={(value) => setSelectedExercise(value)}
+        onValueChange={value => setSelectedExercise(value)}
         items={
-          selectedCategory?.exercises.map((exercise) => ({
+          selectedCategory?.exercises.map(exercise => ({
             label: exercise.name,
             value: exercise.id,
           })) || []
         }
-        style={{textAlign: 'center'}}
+        style={pickerStyles}
         placeholder={{label: 'Select an exercise', value: null}}
-      /> */}
+        Icon={() => <Icon type="material" name="expand-more" size={40} />}
+      />
     </>
   );
 };
--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PageLayout from '../../layouts/PageLayout';
 import {Text} from 'react-native-elements';
 import {StyleSheet} from 'react-native';
-
-import {Item} from '../../components/Dropdown/DropdownItem';
-import Dropdown from '../../components/Dropdown/Dropdown';
 
 const HomeScreen = () => {
   const styles = StyleSheet.create({
@@ -13,60 +10,11 @@ const HomeScreen = () => {
     },
   });
 
-  const [items, setItems] = useState<Item<string>[]>([
-    {
-      label: 'Item1',
-      value: 'Value1',
-    },
-    {
-      label: 'Item2',
-      value: 'Value2',
-    },
-    {
-      label: 'Item3',
-      value: 'Value3',
-    },
-    {
-      label: 'Item4',
-      value: 'Value4',
-    },
-    {
-      label: 'Item5',
-      value: 'Value5',
-    },
-    {
-      label: 'Item6',
-      value: 'Value6',
-    },
-    {
-      label: 'Item7',
-      value: 'Value7',
-    },
-    {
-      label: 'Item8',
-      value: 'Value8',
-    },
-    {
-      label: 'Item9',
-      value: 'Value9',
-    },
-    {
-      label: 'Item10',
-      value: 'Value10',
-    },
-  ]);
-  const [selectedItem, setSelectedItem] = useState<string>();
-
   return (
     <PageLayout loading={false}>
       <Text style={styles.header} h4>
         Home
       </Text>
-      <Dropdown
-        items={items}
-        selectedItem={selectedItem}
-        setSelectedItem={setSelectedItem}
-      />
     </PageLayout>
   );
 };
