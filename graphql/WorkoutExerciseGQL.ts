50dc75c725d10b8cfe8f08013fa25fd5d5972168
Nick Coffey <njc3n3@gmail.com>
2020-04-18 07:42:40 -0500

    added workout exercise and set GQL

--- graphql/WorkoutExerciseGQL.ts
+++ graphql/WorkoutExerciseGQL.ts
@@ -1,32 +1,75 @@
-import {gql} from 'apollo-boost';
+import {gql} from 'apollo-boost'
+import {Workout} from './WorkoutGQL'
+import {WorkoutSet} from './WorkoutSetGQL'
 
 export const ADD_WORKOUT_EXERCISE = gql`
-  mutation addWorkoutExercise(
-    $newWorkoutExercise: CreateWorkoutExerciseInput!
-  ) {
+  mutation addWorkoutExercise($newWorkoutExercise: CreateWorkoutExerciseInput!) {
     addWorkoutExercise(workoutExercise: $newWorkoutExercise) {
       id
       name
       desc
     }
   }
-`;
+`
 
-export type WorkoutExercise = {
-  id: string;
-  name: string;
-  desc: string;
-};
+export const UPDATE_WORKOUT_EXERCISE = gql`
+  mutation updateWorkoutExercise($updatedWorkoutExercise: UpdateWorkoutExerciseInput!) {
+    updateWorkoutExercise(workoutExercise: $updatedWorkoutExercise) {
+      id
+      name
+      desc
+    }
+  }
+`
+
+export const WORKOUT_EXERCISE = gql`
+  query getWorkoutExercise($id: ID!) {
+    workoutExercise(id: $id) {
+      id
+      name
+      desc
+      workout {
+        id
+      }
+    }
+  }
+`
+
+export const REMOVE_WORKOUT_EXERCISE = gql`
+  mutation removeWorkoutExercise($id: ID!) {
+    removeWorkoutExercise(id: $id) {
+      id
+      name
+      desc
+    }
+  }
+`
 
 export type CreateWorkoutExerciseInput = {
-  name: string;
-  desc?: string;
-  workoutId: string;
-};
+  name: string
+  desc?: string
+  workoutId: string
+}
 
 export type UpdateWorkoutExerciseInput = {
-  id: string;
-  name?: string;
-  desc?: string;
-  workoutId?: string;
-};
+  id: string
+  name?: string
+  desc?: string
+  workoutId?: string
+}
+
+export type WorkoutExercise = {
+  id: string
+  name: string
+  desc: string
+  workout: Workout
+  workoutSets: WorkoutSet[]
+}
+
+export type WorkoutExerciseData = {
+  workoutExercise: WorkoutExercise
+}
+
+export type WorkoutExercisesData = {
+  workoutExercises: WorkoutExercise[]
+}
---
+++ graphql/WorkoutSetGQL.ts
@@ -0,0 +1,73 @@
+import {gql} from 'apollo-boost'
+import {WorkoutExercise} from './WorkoutExerciseGQL'
+
+export const ADD_WORKOUT_SET = gql`
+  mutation addWorkoutSet($newWorkoutSet: WorkoutSetCreateInput!) {
+    addWorkoutSet(workoutSet: $newWorkoutSet) {
+      id
+      weight
+      repetitions
+    }
+  }
+`
+
+export const UPDATE_WORKOUT_SET = gql`
+  mutation updateWorkoutSet($updatedWorkoutSet: WorkoutSetUpdateInput!) {
+    updateWorkoutSet(workoutSet: $updatedWorkoutSet) {
+      id
+      weight
+      repetitions
+    }
+  }
+`
+
+export const WORKOUT_SET = gql`
+  query getWorkoutSet($id: ID!) {
+    workoutSet(id: $id) {
+      id
+      weight
+      repetitions
+      workoutExercise {
+        id
+      }
+    }
+  }
+`
+
+export const REMOVE_WORKOUT_SET = gql`
+  mutation removeWorkoutSet($id: ID!) {
+    removeWorkoutSet(id: $id) {
+      id
+      weight
+      repetitions
+    }
+  }
+`
+
+export type WorkoutSetCreateInput = {
+  weight: string
+  repetitions?: string
+  workoutExerciseId: string
+}
+
+export type WorkoutSetUpdateInput = {
+  id: string
+  weight?: string
+  repetitions?: string
+  workoutExerciseId: string
+}
+
+export type WorkoutSet = {
+  id: string
+  weight: string
+  repetitions: string
+  workoutExercise: WorkoutExercise
+}
+
+export type WorkoutSetData = {
+  workoutSet: WorkoutSet
+}
+
+export type WorkoutSetsData = {
+  workoutSets: WorkoutSet[]
+}
