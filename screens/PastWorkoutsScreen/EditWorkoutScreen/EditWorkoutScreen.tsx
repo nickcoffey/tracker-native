73bbb512faa2ff02b5d99db330a134b41950a39d
Nick Coffey <njc3n3@gmail.com>
2020-05-05 12:30:30 -0500

    added start and end time to past workout screen

--- screens/PastWorkoutsScreen/EditWorkoutScreen/EditWorkoutScreen.tsx
+++ screens/PastWorkoutsScreen/EditWorkoutScreen/EditWorkoutScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {StyleSheet, View} from 'react-native'
+import {Text, Icon} from 'react-native-elements'
 import {RouteProp} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import {useQuery} from '@apollo/react-hooks'
 
 import {PastWorkoutsStackParamList} from '../PastWorkoutsNavigator'
 import WorkoutPage from '../../../components/Workout/WorkoutPage'
+import StyledDivider from '../../../components/StyledDivider'
+import {getFormattedTime, getFormattedDate} from '../../../utils/DateUtils'
 import {WorkoutDataWithExercises, WORKOUT_WITH_EXERCISES} from '../../../graphql/WorkoutGQL'
 import {WorkoutExercise} from '../../../graphql/WorkoutExerciseGQL'
 
@@ -31,15 +35,64 @@ const EditWorkoutScreen = ({navigation, route}: Props) => {
     })
   }
 
+  let formattedStartDate: string | undefined,
+    formattedStartTime: string | undefined,
+    formattedEndDate: string | undefined,
+    formattedEndTime: string | undefined
+
+  if (data?.workout.startTime) {
+    const startTime = data.workout.startTime
+    formattedStartDate = getFormattedDate(startTime)
+    formattedStartTime = getFormattedTime(startTime)
+  }
+  if (data?.workout.endTime) {
+    const endTime = data.workout.endTime
+    formattedEndDate = getFormattedDate(endTime)
+    formattedEndTime = getFormattedTime(endTime)
+  }
+  const doDisplayEndDate =
+    Boolean(formattedStartDate) && Boolean(formattedEndDate) && formattedStartDate !== formattedEndDate
+  console.log(doDisplayEndDate)
+
+  const Arrow = <Icon type='material' name='trending-flat' />
+
   return (
     <WorkoutPage
       loading={loading}
       refetch={refetch}
       refreshWorkout={refreshWorkout}
       handleExercisePress={handleExercisePress}
-      workout={data?.workout}
-    />
+      workout={data?.workout}>
+      <StyledDivider />
+      <View style={styles.header}>
+        <Text style={{...styles.headerText, ...styles.dateHeader}}>{formattedStartDate}</Text>
+        {doDisplayEndDate && <Text style={{...styles.headerText, ...styles.dateHeader}}>{formattedEndDate}</Text>}
+      </View>
+      {doDisplayEndDate ? Arrow : <StyledDivider size={5} />}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>{formattedStartTime}</Text>
+        {!doDisplayEndDate && Arrow}
+        <Text style={styles.headerText}>{formattedEndTime || 'Not finished'}</Text>
+      </View>
+    </WorkoutPage>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'space-around'
+  },
+  headerText: {
+    textAlign: 'center',
+    flex: 1,
+    fontSize: 18
+  },
+  dateHeader: {
+    textDecorationLine: 'underline'
+  }
+})
 
 export default EditWorkoutScreen
