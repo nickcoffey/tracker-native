f931b01765f95fe8a92e695f1486f4b67870b5b8
Nick Coffey <njc3n3@gmail.com>
2020-05-06 16:48:26 -0500

    added dark mode to list items

--- components/EditableListItem.tsx
+++ components/EditableListItem.tsx
@@ -1,8 +1,10 @@
 import React, {ReactElement} from 'react'
 import {View, StyleSheet} from 'react-native'
 import {ListItem} from 'react-native-elements'
+import {useTheme} from '@react-navigation/native'
 
 import StyledButton from './StyledButton'
+// import StyledDivider from './StyledDivider'
 
 type Props = {
   isEditing: boolean
@@ -32,7 +34,10 @@ const EditableListItem = ({
   const handleCancelPress = () => setIsEditing(false)
   const handleLongPress = () => setIsEditing(true)
 
+  const {colors} = useTheme()
   const styles = StyleSheet.create({
+    item: {backgroundColor: colors.border},
+    itemText: {color: colors.text},
     btnGroup: {
       flex: 1,
       flexDirection: 'row',
@@ -41,28 +46,34 @@ const EditableListItem = ({
   })
 
   return (
-    <ListItem
-      title={isEditing ? titleEditMode : title}
-      subtitle={
-        isEditing ? (
-          <>
-            {subtitleEditMode}
-            <View style={styles.btnGroup}>
-              <StyledButton title='Delete' onPress={createDeleteAlert} isDelete />
-              <StyledButton title='Update' onPress={handleUpdate} />
-              <StyledButton title='Cancel' onPress={handleCancelPress} isCancel />
-            </View>
-          </>
-        ) : (
-          subtitle
-        )
-      }
-      topDivider={topDivider}
-      onPress={handlePress}
-      onLongPress={handleLongPress}
-      bottomDivider
-      chevron
-    />
+    <>
+      <ListItem
+        title={isEditing ? titleEditMode : title}
+        subtitle={
+          isEditing ? (
+            <>
+              {subtitleEditMode}
+              <View style={styles.btnGroup}>
+                <StyledButton title='Delete' onPress={createDeleteAlert} isDelete />
+                <StyledButton title='Update' onPress={handleUpdate} />
+                <StyledButton title='Cancel' onPress={handleCancelPress} isCancel />
+              </View>
+            </>
+          ) : (
+            subtitle
+          )
+        }
+        titleStyle={styles.itemText}
+        subtitleStyle={styles.itemText}
+        containerStyle={styles.item}
+        topDivider={topDivider}
+        onPress={handlePress}
+        onLongPress={handleLongPress}
+        bottomDivider
+        chevron
+      />
+      {/* <StyledDivider color={colors.background} size={5} /> */}
+    </>
   )
 }
 
