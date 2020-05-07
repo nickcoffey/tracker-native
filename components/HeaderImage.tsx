86230be67bf2b7416837f62bfb94d9226b2b7049
Nick Coffey <njc3n3@gmail.com>
2020-05-07 16:37:42 -0500

    added dark mode header icon

--- components/HeaderImage.tsx
+++ components/HeaderImage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import {Image} from 'react-native-elements'
+import {Appearance} from 'react-native-appearance'
 
 const HeaderImage = () => (
   <Image
-    source={require('../assets/tracker-transparent.png')}
+    source={
+      Appearance.getColorScheme() === 'dark'
+        ? require('../assets/tracker-dark-transparent.png')
+        : require('../assets/tracker-transparent.png')
+    }
     placeholderStyle={{backgroundColor: 'rgba(0, 0, 0, 0)'}}
     style={{width: 150, height: 75}}
   />
